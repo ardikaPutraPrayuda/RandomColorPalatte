96c3f9c4dadd96e0d369cb3837cd2397b4940998
ardikaPutraPrayuda <ardikaputraa22@gmail.com>
2021-05-03 11:50:11 +0700

    change

--- style.js
+++ style.js
@@ -8,7 +8,7 @@ const btn = document.querySelector(".btn");
 const randomColorBlock = document.querySelectorAll(".box");
 
 function RandomHexColorCode() {
-  var chars = "0123456789abcdef";
+  var chars = "0123456789ABCDEF";
   var colorLength = 6;
   var color = "";
 
